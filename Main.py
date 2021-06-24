263acb860c73c2a6a0aea889b6d445d7c3ef77e5
Regan Ross <rross@laurentian.ca>
2021-06-24 15:50:52 -0400

    added arguments, functional and printing to txt file

--- Main.py
+++ Main.py
@@ -2,6 +2,18 @@ import numpy as np
 from Elements import Muon as mu
 from Elements import Generator as gen
 from Elements import Detector as det
+import csv
+import argparse #For future options
+
+
+### Commandline Args ###
+parser = argparse.ArgumentParser(description='Necessary Arguments')
+parser.add_argument('numOfMuons',\
+ help="The integer number of muons to generate in csv", type=int,\
+ action='store', default=1)
+parser.add_argument('outputFile', action='store',\
+ help="The name of the csv file in which to output.", nargs='?', type=str, default="muonGenOutput.txt")
+args = parser.parse_args()
 
 
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
@@ -30,10 +42,13 @@ MuonGen = gen.Generator(Gen_Radius, Gen_Position)
 print(str(MuonGen))
 
 
+### Open File ###
+
+outputFile = open(args.outputFile, 'a')
+
+
 ### Generate Muons ###
-numMuons = 10
+muons = MuonGen.generateMuons(args.numOfMuons)
 
-muons = MuonGen.generateMuons(numMuons)
-
-for i in range(numMuons):
-    print(str(muons[i]))
+for i in range(args.numOfMuons):
+    outputFile.write(str(muons[i]))
