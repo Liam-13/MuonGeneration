fixed units

Elements/Generator.py
@@ -18,7 +18,7 @@ class Generator(object):
     '''
 
     def __init__(self, radius, position = np.array([0.,0.,10.])):
-        self.radius = radius
+        self.radius = radius # meters
         self.position = position
         self.resolution = 1000
 